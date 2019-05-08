c4b2f1613e53a6ead509403f65a4caf95592dc29
REZA AKIL <akil.reza@gmail.com>
2019-05-08 18:14:21 +0200

    mesage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import DogsListContainer from './components/DogsListContainer'
+import { Route } from 'react-router-dom'
+//import DogBreedImages from './components/DogBreedImages'
+import DogBreedImagesContainer from './components/DogBreedImagesContainer'
+
 
 
 class App extends Component {
@@ -10,7 +14,9 @@ class App extends Component {
     <div className="App">
       <main>
         
-        <DogsListContainer />
+        <Route exact path="/" component={DogsListContainer} />
+        <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
+
 
       </main>
 
---
+++ src/components/DogBreedImages.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+//import DogBreedImagesContainer from './DogBreedImagesContainer'
+
+
+export default function DogBreedImages (props) {
+    console.log(props.images)
+    
+  return (
+    <div className="dog-breed-images">
+      <h1>Dogs Breed Images</h1>
+
+      <Link to="/">Go back to the index</Link>
+      <div>
+     { props.images && props.images.map(url => <img src={ url } alt="Dog" />) }
+     { !props.images && 'Loading...' }
+</div>
+      
+    </div>
+  )
+}
